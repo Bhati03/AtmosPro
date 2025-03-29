964602b026d4fc25479c9ff2a2515dc97ba202fe
Bhati03 <140142366+Bhati03@users.noreply.github.com>
2025-03-29 18:13:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
     port: process.env.PORT || 5173,
     host: "0.0.0.0",
     strictPort: true,
-    allowedHosts: ["https://atmospro.onrender.com"], // Add this line
+    allowedHosts: ["atmospro.onrender.com"], // Add this line
   },
 });
